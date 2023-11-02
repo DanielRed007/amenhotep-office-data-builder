40ef37d982de5e04b7df106dd6881380a4c14228
danielred007 <danielrojo1927@gmail.com>
2023-11-02 23:10:00 +0000

    New More Structured SQL + Node Approach

--- express-server/builder-postgres/src/route/building.route.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getBuildings } from '../controller/building.controller';
-
-const router = express.Router();
-
-router.get('/', getBuildings);
-
-export default router;
