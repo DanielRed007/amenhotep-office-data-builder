8cd8caf5254853741f16c197550a49bb0fc1653c
danielred007 <danielrojo1927@gmail.com>
2023-12-04 08:59:41 +0000

    current advance on db migrationjs

--- express-server/builder-postgres/models/building.model.ts
+++ express-server/builder-postgres/models/building.model.ts
@@ -1,63 +1,60 @@
-import { Model, Table, Column, DataType } from "sequelize-typescript";
+import { Model, Table, Column, DataType, HasMany } from 'sequelize-typescript';
+import Floor from './floor.model';
 
 @Table({
-  tableName: "buildings",
-  timestamps: false, 
+  tableName: 'buildings',
+  timestamps: false,
 })
 export default class Building extends Model {
   @Column({
     type: DataType.STRING,
     primaryKey: true,
-    field: "id"
+    field: 'id',
   })
   id?: string;
 
   @Column({
     type: DataType.STRING(255),
-    field: "name"
+    field: 'name',
   })
   name?: string;
 
   @Column({
     type: DataType.STRING(255),
-    field: "nickname"
+    field: 'nickname',
   })
   nickname?: string;
 
-  @Column({
-    type: DataType.ARRAY(DataType.STRING),
-    field: "floors",
-    allowNull: true
-  })
-  floors?: string[];
+  @HasMany(() => Floor)
+  floors?: Floor[];
 
   @Column({
     type: DataType.STRING,
-    field: "status"
+    field: 'status',
   })
   status?: string;
 
   @Column({
     type: DataType.STRING,
-    field: "type"
+    field: 'type',
   })
   type?: string;
 
   @Column({
     type: DataType.STRING(255),
-    field: "address"
+    field: 'address',
   })
   address?: string;
 
   @Column({
     type: DataType.STRING,
-    field: "city"
+    field: 'city',
   })
   city?: string;
 
   @Column({
     type: DataType.STRING,
-    field: "country"
+    field: 'country',
   })
   country?: string;
 }
